a32d548326d7e9697385b2ec55830d3b4d3c6858
kelvin duan <kelvin_duan@epam.com>
2023-05-17 15:54:19 +0800

    init project

--- server.js
+++ server.js
@@ -151,6 +151,6 @@ app.post("/dashboard", encodeUrl, (req, res)=>{
     });
 });
 
-app.listen(4000, ()=>{
-    console.log("Server running on port 4000");
+app.listen(3000, ()=>{
+    console.log("Server running on port 3000");
 });
